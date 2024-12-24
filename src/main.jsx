5824c9c860d0c1f621f636ca917bb9794918e3ff
YashParsana2004 <yashparsana812@gmail.com>
2024-12-24 13:19:12 +0530

    New file

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Component1 from './lec24_12/component1'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Component1/>
+  </StrictMode>,
+)
