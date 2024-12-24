9122a7f752d45a769de98b5d9b01232ace4d9f47
YashParsana2004 <yashparsana812@gmail.com>
2024-12-24 13:32:05 +0530

    Nesting

--- src/lec24_12/component1.jsx
+++ src/lec24_12/component1.jsx
@@ -1,11 +1,29 @@
 import * as React from "react";
 
+class Component2 extends React.Component {
+    render() {
+        return <div>
+            <Component3/>
+            <h3>Component 2</h3>
+        </div>
+    }
+}
+
+class Component3 extends React.Component {
+    render() {
+        return <h2>Component 3</h2>;
+    }
+}
+
+
 class Component1 extends React.Component {
     render() {
         return (
             <div>
+                <Component2 />
+                
                 <h1>Component 1</h1>
-                <h2>Component 2</h2>
+                <h2>Hello..!!</h2>
             </div>
         );
     }
