a328e6d4829c0d382b0c9c8f329709b7d1638f44
YashParsana2004 <yashparsana812@gmail.com>
2024-12-30 09:47:56 +0530

    State Management

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import StateABC from './lec29_12/statemanagement1'
 //import Timetable from './lec24_12/timetale'
-import TimetableComp from './lec24_12/timetale'
+//import TimetableComp from './lec24_12/timetale'
 // import Component1 from './lec24_12/component1'
 // import HtmlComponent from './lec24_12/htmlcomp'
 
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <Component1/> */}
     {/* <HtmlComponent/> */}
-    <TimetableComp/>
+    {/* <TimetableComp/> */}
+    <StateABC/>
   </StrictMode>,
 )
